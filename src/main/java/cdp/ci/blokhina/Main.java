ecf309d5dfd1b9b4ee1272cf54ba8c53ad18a336
Bohdan_Krasin <krasincos@gmail.com>
2015-10-14 22:50:18 +0300

    Initital Commit

---
+++ src/main/java/cdp/ci/blokhina/Main.java
@@ -0,0 +1,21 @@
+package cdp.ci.blokhina;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Main {
+    private static final String URL = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
+
+    public static void main(String[] args) throws IOException {
+        URL url = new URL(URL);
+        try (ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+             FileOutputStream fos = new FileOutputStream("target\\File.txt")) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        } catch (IOException e) {
+            System.out.println("Something was wrong during the download");
+        }
+    }
+}
